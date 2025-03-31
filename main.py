fcebaf08ce78f46d1e2a3a381770e1404e2fd07e
Pretnaro <pretnarozbej@gmail.com>
2025-03-31 07:31:35 +0200

    working slots and horse races

--- main.py
+++ main.py
@@ -11,34 +11,6 @@ messages = db.table('sporocila')
 User = Query()
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        try:
-            username = request.form['username']
-            password = request.form['password']
-            user = users.get(User.username == username)
-            
-            if user:
-                if user['password'] == password:
-                    session['username'] = username
-                    return jsonify({'success': True})
-                else:
-                    return jsonify({'success': False, 'error': 'Napačno geslo'})
-            else:
-                users.insert({'username': username, 'password': password})
-                session['username'] = username
-                return jsonify({'success': True})
-        except Exception as e:
-            print(f"Napaka pri prijavi: {str(e)}")
-            return jsonify({'success': False, 'error': 'Prišlo je do napake'})
-    return render_template('login.html')
-
-@app.route('/logout')
-def logout():  
-    session.pop('username', None)
-    return redirect(url_for('login'))
-
 @app.route("/index")
 def index():
     return render_template("index.html")
