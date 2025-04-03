511b9d42fe4d737ad3373f5b0bcf1d9ee97c2755
pretnaro <pretnarozbej@gmail.com>
2025-04-03 14:15:39 +0200

    main

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def blackjack():
     return render_template("blackjack.html")
 
 
-#PODATKOVNI ROUTE KOVANEC
+#PODATKOVNI ROUTE BLACKJACK
 @app.route("/blackjackGet")
 def getBlackjack():
     #BET INPUT
@@ -224,6 +224,161 @@ def getBlackjack():
                 elif dealerCards < playerCard2 and dealerCards < playerCard2:
                     print("NONE")
     print(f"Your new balance is: {balance}")  
+
+#SLOTS
+@app.route("/slots")
+def slots():
+    return render_template("slots.html")
+
+
+#PODATKOVNI ROUTE SLOTS
+@app.route("/slotsGet")
+def getSlots():
+    rnd1=random.randrange(1,10)
+    rnd2=random.randrange(1,10)
+    rnd3=random.randrange(1,10)
+    print(rnd1, rnd2, rnd3)
+    if rnd1 == rnd2 == rnd3 == 1:
+    
+        print("YOU WON 1€")
+    if rnd1 == rnd2 == rnd3 == 2:
+        print("YOU WON 20€")
+    if rnd1 == rnd2 == rnd3 == 3:
+        print("YOU WON 30€")
+    if rnd1 == rnd2 == rnd3 == 4:
+        print("YOU WON 40€")
+    if rnd1 == rnd2 == rnd3 == 5:
+        print("YOU WON 500€")
+    if rnd1 == rnd2 == rnd3 == 6:
+        print("YOU WON 60€")
+    if rnd1 == rnd2 == rnd3 == 7:
+        print("YOU WON 1000€")
+    if rnd1 == rnd2 == rnd3 == 8:
+        print("YOU WON 80€")
+    if rnd1 == rnd2 == rnd3 == 9:
+        print("YOU WON 90€")
+    else:
+        print("POSKUSI PONOVNO")
+
+
+#ROLETA
+@app.route("/roleta")
+def roleta():
+    return render_template("roleta.html")
+
+
+#PODATKOVNI ROUTE ROLETA
+@app.route("/roletaGet")
+def getRoleta():
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"]
+    firstThird = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+    secondThird = ["13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
+    thirdThird = ["25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"]
+    odd = ["1", "3", "5", "7", "9", "11", "13", "15", "17", "19", "21", "23", "25", "27", "29", "31", "33", "35"]
+    even = ["2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32", "34", "36"]
+    red = ["1", "3", "5", "7", "9", "12", "14", "16", "18", "19", "21", "23", "25", "27", "30", "32", "34", "36"]
+    black = ["2", "4", "6", "8", "10", "11", "13", "15", "17", "20", "22", "24", "26", "29", "31", "33", "35"]
+    firstHalf = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"]
+    secondHald = ["19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"]
+    firstTol = ["1", "4", "7", "10", "13", "16", "19", "22", "25", "28", "31", "34"]
+    secondTol = ["2", "5", "8", "11", "14", "17", "20", "23", "26", "29", "32", "35"]
+    thirdTol = ["3", "6", "9", "12", "15", "18", "21", "24", "27", "30", "33", "36"]
+    print("Your choices are: input number, type: firstthird, secondthird, thirdthird, odd, even, red, black, firsthalf, secondhalf, firsttol, secondtol or thirdtol")
+    choice = str(input("Input choice you want to play with: "))
+    #SPIN
+    number = random.randrange(0,37)
+    number = str(number)
+    print(f"NUMBER IS {number}")
+    #NUMBER
+    if choice in numbers: 
+        if choice == number:
+            print("WIN")
+        else:
+            print("LOST")
+    #FIRST THIRD
+    elif choice == "firstthird":
+        if number in firstThird:
+            print("WIN")
+        else:
+            print("LOST")
+    #SECOND THIRD
+    elif choice == "secondthird":
+        if number in secondThird:
+            print("WIN")
+        else:
+            print("LOST")
+    #THIRD THIRD
+    elif choice == "thirdthird":
+        if number in thirdThird:
+            print("WIN")
+        else:
+            print("LOST")
+    #ODD
+    elif choice == "odd":
+        if number in odd:
+            print("WIN")
+        else:
+            print("LOST")
+    #EVEN
+    elif choice == "even":
+        if number in even:
+            print("WIN")
+        else:
+            print("LOST")
+    #RED
+    elif choice == "red":
+        if number in red:
+            print("WIN")
+        else:
+            print("LOST")
+    #BLACK
+    elif choice == "black":
+        if number in black:
+            print("WIN")
+        else:
+            print("LOST")
+    #FIRST HALF
+    elif choice == "firsthalf":
+        if number in black:
+            print("WIN")
+        else:
+            print("LOST")
+    #SECOND HALF
+    elif choice == "secondhalf":
+        if number in black:
+            print("WIN")
+        else:
+            print("LOST")
+    #FIRST STOL
+    elif choice == "firsttol":
+        if number in black:
+            print("WIN")
+        else:
+            print("LOST")
+    #SECOND STOL
+    elif choice == "secondtol":
+        if number in black:
+            print("WIN")
+        else:
+            print("LOST")
+    #THIRD STOL
+    elif choice == "thirdtol":
+        if number in black:
+            print("WIN")
+        else:
+            print("LOST")
+    else:
+        print("THIS IS NOT AN OPTION")
+
+#ROLETA
+@app.route("/horseraces")
+def horseraces():
+    return render_template("horseraces.html")
+
+
+#PODATKOVNI ROUTE ROLETA
+@app.route("/horceracesGet")
+def getHorseraces():
  
 
 app.run(debug = True)
