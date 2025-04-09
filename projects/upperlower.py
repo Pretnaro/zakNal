d4391cab748631490ddca7686d4f3e4b80acce9d
pretnaro <pretnarozbej@gmail.com>
2025-04-09 09:59:16 +0200

    upperlower

---
+++ projects/upperlower.py
@@ -0,0 +1,45 @@
+import random
+card1 = random.randint(1, 14)
+card2 = random.randint(1, 14)
+card3 = random.randint(1, 14)
+card4 = random.randint(1, 14)
+
+
+while True:
+    print(card1)
+
+    guess1 = input("U/L: ")
+    print(card2)
+
+    if card2 == card1:
+        print("isti karti")
+        break
+
+    if (card2 > card1 and guess1 == "U") or (card2 < card1 and guess1 == "L"):
+        guess2 = input("U/L: ")
+        print(card3)
+
+        if card3 == card2:
+            print("isti karti")
+            break
+
+        if (card3 > card2 and guess2 == "U") or (card3 < card2 and guess2 == "L"):
+            guess3 = input("U/L: ")
+            print(card4)
+
+            if card4 == card3:
+                print("isti karti")
+                break
+
+            if (card4 > card3 and guess3 == "U") or (card4 < card3 and guess3 == "L"):
+                print("WIN")
+                break
+            else:
+                print("BUST")
+                break
+        else:
+            print("BUST")
+            break
+    else:
+        print("BUST")
+        break
