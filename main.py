5552caeacc52f89aada9e6509817ed57775d4a6e
pretnaro <pretnarozbej@gmail.com>
2025-04-10 16:44:04 +0200

    name

--- main.py
+++ main.py
@@ -457,25 +457,25 @@ def getUpperlower():
 
 
     result = "LOST"
-    card1 = random.randint(1, 14)
-    card2 = random.randint(1, 14)
-    card3 = random.randint(1, 14)
-    card4 = random.randint(1, 14)
+    card1 = random.randint(1, 13)
+    card2 = random.randint(1, 13)
+    card3 = random.randint(1, 13)
+    card4 = random.randint(1, 13)
     while True:
         print(card1)
-        guess1 = input("U/L: ")
+        guess1 = request.args.get("U/L: ")
         print(card2)
         if card2 == card1:
             result = "isti karti"
             break
         if (card2 > card1 and guess1 == "U") or (card2 < card1 and guess1 == "L"):
-            guess2 = input("U/L: ")
+            guess1 = request.args.get("U/L: ")
             print(card3)
             if card3 == card2:
                 result = "isti karti"
                 break
             if (card3 > card2 and guess2 == "U") or (card3 < card2 and guess2 == "L"):
-                guess3 = input("U/L: ")
+                guess1 = request.args.get("U/L: ")
                 print(card4)
                 if card4 == card3:
                     result = "isti karti"
